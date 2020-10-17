38b36e61b80cba69a88a75c7545d02ba95b8264a
JustinDL <justin_d_lucas@hotmail.com>
2020-10-16 20:54:35 -0400

    basic express setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+
 import 'dotenv/config';
 
-console.log('hello api how are you?');
+import express from 'express';
 
-console.log(process.env.JWT_SECRET);
+const app = express();
+
+// how to call from env file
+// port in env file because it is hidden from front end
+const PORT = process.env.PORT;
+
+//routes in express
+app.get('/', (req, res) => {
+
+    res.send('Hello API');
+    
+});
+
+
+
+// web based aps are on port 80
+app.listen(process.env.PORT, () => {
+    console.log(`Week 4 is running on port ${process.env.PORT}!`);
+});
